aa1b85606fa6cd2e0befcac889b1a0495fe0bb2d
PendoKenneth <95934335+PendoKenneth@users.noreply.github.com>
2023-09-27 12:17:25 +0100

    Update pendo.js

--- pendo.js
+++ pendo.js
@@ -22,7 +22,7 @@
             },
 
             account: {
-                id:           '*****L1_KCH_NHS_Foundation_Trust', //'PendoAllStar123', // Required if using Pendo Feedback
+                id:           'PendoAllStar123', // Required if using Pendo Feedback
                 name:         '', // Optional
                 is_paying:    'True', // Recommended if using Pendo Feedback
                 monthly_value:  '500,000', // Recommended if using Pendo Feedback
