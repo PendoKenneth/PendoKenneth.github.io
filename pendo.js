71b109bf34eef445c968e77bcc7448ac31d12d37
PendoKenneth <95934335+PendoKenneth@users.noreply.github.com>
2025-01-21 18:11:31 +0000

    Update pendo.js
    Separating the Pendo install script

--- pendo.js
+++ pendo.js
@@ -4,43 +4,4 @@
         o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
         y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
         z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
-
-        // Call this whenever information about your visitors becomes available
-        // Please use Strings, Numbers, or Bools for value types.
-        pendo.initialize({
-            visitor: {
-                id:            'PU-0000021',   // Required if user is logged in
-                email:         'kenneth.lewis@pendo.io',  // Recommended if using Pendo Feedback, or NPS Email
-                full_name:    'Kenneth Lewis',   // Recommended if using Pendo Feedback
-                firstName:    'Kenneth',
-                surname:      'Lewis',
-                role:         'Sr. CSM', // Optional
-                eventdate:    '2023-07-14'//,
-                //starRating:    'All-Star'
-
-                // You can add any additional visitor level key-values here,
-                // as long as it's not one of the above reserved names.
-            },
-
-            account: {
-                id:           'PendoAllStar123', // Required if using Pendo Feedback
-                //name:         '', // Optional
-                is_paying:    'True', // Recommended if using Pendo Feedback
-                //monthly_value:  '500,000', // Recommended if using Pendo Feedback
-                planLevel:    'Enterprise', // Optional
-                // planPrice:    // Optional
-                creationDate: '2021-02-01'// Optional
-
-                // You can add any additional account level key-values here,
-                // as long as it's not one of the above reserved names.
-            },
-            
-            parentAccount: {
-               // id:           'Pendo',
-               // name:         'Pendo'  // Optional
-
-                // You can add any additional parent account level key-values here,
-                // as long as it's not one of the above reserved names.
-            }
-        });
 })('f2b44630-7489-4192-4efe-33802ab41173');
